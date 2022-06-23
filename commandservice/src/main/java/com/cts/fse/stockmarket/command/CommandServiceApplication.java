aef4414b4bd89b4fb3e4d9148f88e3c91c243956
moretolearn <yanumularammohan661@gmail.com>
2022-06-23 13:01:11 +0530

    Configured Service Registry for Command Service

--- commandservice/src/main/java/com/cts/fse/stockmarket/command/CommandServiceApplication.java
+++ commandservice/src/main/java/com/cts/fse/stockmarket/command/CommandServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,6 +16,7 @@ import com.cts.fse.stockmarket.command.repository.CustomRepositoryImpl;
 @EnableJpaRepositories(repositoryBaseClass = CustomRepositoryImpl.class)
 @EntityScan("com.cts.fse.stockmarket.command.bean")
 @CrossOrigin
+@EnableEurekaClient
 public class CommandServiceApplication
 {
     public static void main(String[] args)
