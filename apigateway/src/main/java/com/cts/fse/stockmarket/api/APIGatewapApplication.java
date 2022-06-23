022ef83f9df368b741b9910c0d6960b663ac70fe
moretolearn <yanumularammohan661@gmail.com>
2022-06-23 12:59:54 +0530

    Added API Gateway for stock market

---
+++ apigateway/src/main/java/com/cts/fse/stockmarket/api/APIGatewapApplication.java
@@ -0,0 +1,19 @@
+package com.cts.fse.stockmarket.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class APIGatewapApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(APIGatewapApplication.class, args);
+	}
+
+}
