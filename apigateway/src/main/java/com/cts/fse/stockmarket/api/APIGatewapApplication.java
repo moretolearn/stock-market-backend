65d7afb5a5086f17a6224056a9fb0513986eb50e
moretolearn <yanumularammohan661@gmail.com>
2022-06-23 14:39:27 +0530

    Issues resolved

--- apigateway/src/main/java/com/cts/fse/stockmarket/api/APIGatewapApplication.java
+++ apigateway/src/main/java/com/cts/fse/stockmarket/api/APIGatewapApplication.java
@@ -3,11 +3,12 @@ package com.cts.fse.stockmarket.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableZuulProxy
 public class APIGatewapApplication {
 
