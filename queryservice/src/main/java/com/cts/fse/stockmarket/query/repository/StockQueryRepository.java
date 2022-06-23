3dcb9883eb9aa2f8205d2d3d339b5202964c0a62
moretolearn <yanumularammohan661@gmail.com>
2022-06-23 14:40:07 +0530

    Issue Resolved

--- queryservice/src/main/java/com/cts/fse/stockmarket/query/repository/StockQueryRepository.java
+++ queryservice/src/main/java/com/cts/fse/stockmarket/query/repository/StockQueryRepository.java
@@ -3,8 +3,6 @@ package com.cts.fse.stockmarket.query.repository;
 import java.util.Date;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
