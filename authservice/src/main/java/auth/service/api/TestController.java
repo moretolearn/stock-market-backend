3dee1ea187038fafe11bd253935736ba02909669
moretolearn <yanumularammohan661@gmail.com>
2022-06-30 18:26:14 +0530

    Added Auth Service Implementation

---
+++ authservice/src/main/java/auth/service/api/TestController.java
@@ -0,0 +1,20 @@
+package auth.service.api;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/data")
+    public ResponseEntity<Principal> get(final Principal principal) {
+        return ResponseEntity.ok(principal);
+    }
+
+}
